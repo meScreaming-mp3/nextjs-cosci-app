c828f4aaa8a1d765e828111e3c78b93e84aabbcd
meScreaming-mp3 <blawniethesecond@gmail.com>
2025-05-27 15:06:19 +0700

    env

--- src/app/components/AppWelcome.tsx
+++ src/app/components/AppWelcome.tsx
@@ -1,19 +1,29 @@
 'use client'
 
+import { Button } from "@/components/ui/button";
+import { useState } from "react";
+
 type AppWelcomeProps = {
     headTitle: string
     isShow: boolean;
 }
 
 export default function AppWelcome({headTitle, isShow}: AppWelcomeProps ){
+    //let title = 'Welcome to cosci';
+    const [title, setTitle] = useState('Welcome to cosci');
     const currentYear = <p>2025</p>;
     const handleClick = () => {
+        //let ~~~
+        setTitle('Welcome to SWU');
         alert('Hello TypeScript')
     }
 
     return (
     <>
         <p>{headTitle}</p>
+        <p>{process.env.NEXT_PUBLIC_APP_NAME}</p>
+        <p>{title.toUpperCase()}</p>
+        <Button onClick={handleClick}>ClickMeplz</Button>
         <button className="bg-blue-700 p-3 m-3 text-white rounded-lg" onClick={handleClick}>ClickMe!</button>
         {currentYear}
         {
@@ -25,3 +35,4 @@ export default function AppWelcome({headTitle, isShow}: AppWelcomeProps ){
     </>
     );
 }
+
