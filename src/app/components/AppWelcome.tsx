49d73b59257d729ef59270a053b23503f58a0659
meScreaming-mp3 <blawniethesecond@gmail.com>
2025-05-27 11:36:36 +0700

    Add Click

---
+++ src/app/components/AppWelcome.tsx
@@ -0,0 +1,24 @@
+'use client'
+
+export default function AppWelcome() {
+    const title = 'Welcome to cosci';
+    const currentYear = <p>2025</p>;
+    const isShow = true
+    const handleClick = () => {
+        alert('Hello TypeScript')
+    }
+
+    return (
+    <>
+        <p>{title.toUpperCase()}</p>
+        <button className="bg-blue-700 p-3 m-3 text-white rounded-lg" onClick={handleClick}>ClickMe!</button>
+        {currentYear}
+        {
+        isShow && <p>Date: 10/10/1998</p>
+        }
+        {
+        isShow ? <p>Hello Next.js</p> : <p>Hello JavaScrip</p>
+        }
+    </>
+    );
+}
