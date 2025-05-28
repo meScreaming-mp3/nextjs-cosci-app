85e998480c839198076a283dbd8ce89092f38c13
meScreaming-mp3 <blawniethesecond@gmail.com>
2025-05-28 14:19:25 +0700

    API

--- src/app/(front)/about/page.tsx
+++ src/app/(front)/about/page.tsx
@@ -1,9 +1,14 @@
 import Contact01Page from "@/components/contact-01/contact-01";
 
-export default function About() {
+export default async function About() {
+    const data = await fetch('https://api.codingthailand.com/api/version', {next: {revalidate:3600} });
+    const apiInfo = await data.json();
     return (
         <>
-            <Contact01Page />
+            <Contact01Page version={apiInfo.data.version} />
+            {
+                apiInfo && <p>{apiInfo.data.version}</p>
+            }
         </>
     );
 }
