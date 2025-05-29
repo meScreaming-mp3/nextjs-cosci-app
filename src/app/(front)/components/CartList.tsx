e79b5e56393a8d5ab8da4a88b159e4cda7ddc82d
meScreaming-mp3 <blawniethesecond@gmail.com>
2025-05-29 15:58:51 +0700

    cartisnice

--- src/app/(front)/components/CartList.tsx
+++ src/app/(front)/components/CartList.tsx
@@ -1,9 +1,71 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 "use client"
 
+import { Button } from "@/components/ui/button";
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+import { useCartStore } from "@/lib/cart-store";
+import { Trash } from "lucide-react";
+import { useRouter } from "next/navigation";
+
 export default function CartList() {
+    const router = useRouter();
+
+    const items = useCartStore((state) => state.items);
+    const removeItem = useCartStore((state) => state.removeItem);
+    const clearCart = useCartStore((state) => state.clearCart);
+    const totalPrice = useCartStore((state) => state.totalPrice());
+    
+    if (items.length === 0) {
+        return <div className="text-center mt-20">ตะกร้าสินค้าว่างเปล่า...</div>
+    }
+
   return (
-    <div className="mx-auto max-w-4xl mt-50">
-      <p>มาบูฮาย</p>
+    <div className="mx-auto max-w-4xl mt-20">
+      <h1 className="text-xl mb-4">ตะกร้าสินค้า</h1>
+      <Table>
+        <TableHeader>
+            <TableRow>
+                <TableHead>รหัสสินค้า</TableHead>
+                <TableHead>ชื่อสินค้า</TableHead>
+                <TableHead>ราคาสินค้า</TableHead>
+                <TableHead>จำนวน</TableHead>
+                <TableHead>รวม</TableHead>
+                <TableHead>เครื่องมือ</TableHead>
+            </TableRow>
+        </TableHeader>
+        <TableBody>
+            {
+                items.map((i) => (
+                    <TableRow key={i.productId}>
+                        <TableCell>{i.productId}</TableCell>
+                        <TableCell>{i.title}</TableCell>
+                        <TableCell>{i.price}</TableCell>
+                        <TableCell>{i.qty}</TableCell>
+                        <TableCell>{i.price * i.qty}</TableCell>
+                        <TableCell>
+                            <Button variant="destructive" onClick={() => { removeItem(i.productId) }}>
+                                <Trash />
+                            </Button>
+                        </TableCell>
+                    </TableRow>
+                ))
+            }
+        </TableBody>
+      </Table>
+        
+        <div className="">
+            <div>
+                รวมทั้งหมด: {totalPrice}
+            </div>
+            <div className="m-4">
+             <Button className="mr-4" variant="outline" onClick={() => { clearCart(); }}>ลบสินค้าทั้งหมด</Button>
+             <Button onClick={() => {
+                clearCart();
+                router.replace('/product')
+             } }>ยืนยันคำสั่งซื้อ</Button>
+            </div>
+        </div>
+      
     </div>
   );
 }
