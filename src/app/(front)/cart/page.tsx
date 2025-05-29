cb8754eee3c97a89929e5ffdaf590f09ed41146e
meScreaming-mp3 <blawniethesecond@gmail.com>
2025-05-29 14:38:16 +0700

    cartcanclicklaew(1)

--- src/app/(front)/cart/page.tsx
+++ src/app/(front)/cart/page.tsx
@@ -1,7 +1,9 @@
+import CartList from "../components/CartList";
+
 export default function Cart() {
   return (
     <>
-      <p>hello cart</p>
+      <CartList />
     </>
   );
 }
---
+++ src/app/(front)/components/CartList.tsx
@@ -0,0 +1,9 @@
+"use client"
+
+export default function CartList() {
+  return (
+    <div className="mx-auto max-w-4xl mt-50">
+      <p>มาบูฮาย</p>
+    </div>
+  );
+}
